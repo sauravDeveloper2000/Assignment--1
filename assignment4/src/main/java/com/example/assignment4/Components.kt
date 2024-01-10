cfdf49beda7709d2082ddcf1af092c337b9c24a6
AckremanLevi <101544495+AckremanLevi@users.noreply.github.com>
2024-01-10 19:36:41 +0530

    Ninth Commit, Assignment4 complete, with minor changes in error screen

--- assignment4/src/main/java/com/example/assignment4/Components.kt
+++ assignment4/src/main/java/com/example/assignment4/Components.kt
@@ -1,7 +1,12 @@
 package com.example.assignment4
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.LazyGridItemScope
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.material3.CircularProgressIndicator
@@ -10,7 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
+import coil.compose.AsyncImagePainter.State.Empty.painter
 
 inline fun <T : Any> LazyGridScope.items(
     items: LazyPagingItems<T>,
@@ -37,14 +44,20 @@ fun OnErrorOne(
     Box(
         modifier = modifier
     ) {
-        Image(
+        Column(
             modifier = Modifier.align(Alignment.Center),
-            painter = painterResource(id = R.drawable.no_internet),
-            contentDescription = null
-        )
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "No Internet Connection."
-        )
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.size(64.dp),
+                painter = painterResource(id = R.drawable.no_internet),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "No Internet Connection."
+            )
+        }
     }
 }
